ab52eba328c76132730d71bad4391ac3bff5a28d
vira.murray <vira.avramenko@gmail.com>
2022-01-12 15:38:14 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+const siteButton = document.querySelector(".sitemap");
+
+// siteButton.addEventListener('click', function(){
+
+// })
+
+	     
+
+    siteButton.onclick = function() {
+
+        document.location.href = 'https://en.wikipedia.org/wiki/Lubny'
+
+            }
+
